eff6624f77891c5b5b136833ea00217a2319872a
sandraTL <sandra.therrien.laperriere@gmail.com>
2016-09-27 14:35:57 -0400

    doc

---
+++ R/Script_Agilent.R
@@ -0,0 +1,14 @@
+#
+#
+# #shin <- inputXLSXtoDF("/Users/sandra/Desktop/Metabolomique/DataSets/Shin&al./PathQuant_Input.xlsx")
+#
+# ##to get an overview of the dataset on a map
+# heatmapAsso("hsa01100", shin, F)
+#
+# ## or you can heatmapAsso("hsa01100", shin, T)
+#
+# ##to have detailled information about what associations mapped on the KEGG' map
+#
+# getDistanceAsso("hsa01100", shin, F,T)
+
+
